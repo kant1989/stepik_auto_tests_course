218d65ca6b8811b17e92ede7e1c67bfd6a87c5da
kant1989 <kkoshiev@gmail.com>
2023-12-17 11:34:13 +0300

    pytest тесты с маркировкой

---
+++ section3/test_fixture8_mark2_inverse_union.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1:
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    @pytest.mark.smoke
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+# запуск тестов без маркировки smoke: pytest -s -v -m "not smoke" test_fixture8_mark2_inverse_union.py
+# запуск тестов с маркировкой smoke и win10: pytest -s -v -m "smoke and win10" test_fixture8_mark2_inverse_union.py
